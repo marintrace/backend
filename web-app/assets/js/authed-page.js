54cf5cad4833f89040bb81c8306d81689bbab37c
Beck L <11636145+blorsch@users.noreply.github.com>
2020-09-19 16:57:13 -0700

    added mark active check

--- web-app/assets/js/authed-page.js
+++ web-app/assets/js/authed-page.js
@@ -3,3 +3,14 @@ document.getElementById("logout").onclick = function() {logout()};
 function logout() {
     document.location.href = "/web/oauth/logout";
 }
+
+
+$(document).ready(function() {
+  //check if marked current user active
+  var currentUserEmail = "blorsch@ma.org"
+  if (window.localStorage.getItem(currentUserEmail+"_active") != "true") {
+    markUserAsActive().then(function() {
+      window.localStorage.setItem(currentUserEmail+"_active", 'true')
+    });
+  }
+});
